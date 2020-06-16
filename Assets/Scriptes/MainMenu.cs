489535b874a3371cd35d2c41c199cd16679b7712
Petko Alexander <kalinarepina@gmail.com>
2020-06-16 17:01:55 +0300

    add start mode and in-game menu

---
+++ Assets/Scriptes/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button _startButton;
+    [SerializeField] private Button _exitButton;
+
+    private void Awake()
+    {
+        _startButton.onClick.AddListener(OnStartButtonClicked);
+        _exitButton.onClick.AddListener(OnExitButtonClicked);
+    }
+
+    private void OnStartButtonClicked()
+    {
+        MenuManager.MainMenu.SetActive(false);
+        MenuManager.ModeMenu.SetActive(true);
+    }
+    
+    private void OnExitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scriptes/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _mainMenu;
+    [SerializeField] private GameObject _modeMenu;
+
+    private static MenuManager Instance { get; set; }
+
+    public static GameObject MainMenu => Instance._mainMenu;
+
+    public static GameObject ModeMenu => Instance._modeMenu;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
