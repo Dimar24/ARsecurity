769e59f04963218e971566e5ad8d5c3630f3da78
dimar24 <dimamarkovets@gmail.com>
2020-06-23 20:34:28 +0300

    Petko olen

--- Assets/Scriptes/MonoBehaviours/Menu/MenuManager.cs
+++ Assets/Scriptes/MonoBehaviours/Menu/MenuManager.cs
@@ -7,12 +7,15 @@ public class MenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject _mainMenu;
     [SerializeField] private GameObject _modeMenu;
+    [SerializeField] private GameObject _exitPopup;
 
     private static MenuManager Instance { get; set; }
 
     public static GameObject MainMenu => Instance._mainMenu;
 
     public static GameObject ModeMenu => Instance._modeMenu;
+    
+    public static GameObject ExitPopup  => Instance._exitPopup;
 
     private void Awake()
     {
